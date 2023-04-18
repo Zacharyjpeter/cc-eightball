87926f3fe442bbd63fca18f4b9ce26f38510a7fe
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-18 18:49:49 -0500

    completed project using eightball variable

--- applet.js
+++ applet.js
@@ -9,6 +9,7 @@ if (userName) {
   console.log("Bitch, enter your name!");
 }
 
+//output varies. Hey Zachary! You asked when will I get hired as a developer?
 switch (randomNumber) {
   case 0:
     console.log("Its certain");
@@ -38,4 +39,36 @@ switch (randomNumber) {
     console.log("Maybe this shouldnt happen...");
     break;
 }
-//output varies. Hey Zachary! You asked when will I get hired as a developer?
+
+//same project. just a different way to do it
+switch (randomNumber) {
+  case 0:
+    eightBall = "Its certain";
+    break;
+  case 1:
+    eightBall = "Eh. Its pretty much happening";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cant predict right now";
+    break;
+  case 4:
+    eightBall = "AHA! Dont count on it";
+    break;
+  case 5:
+    eightBall = "Sorry bro. My sources say no";
+    break;
+  case 6:
+    eightBall = "Ooo. Its not looking so good";
+    break;
+  case 7:
+    eightBall = "The starts are pointing to yes";
+    break;
+  default:
+    eightBall = "Maybe this shouldnt happen...";
+    break;
+}
+
+console.log(eightBall);
